7aa8bc1194c6d16f9b274ccb42070d0d8015c251
Steven Gardiner <sjgardiner@ucdavis.edu>
2015-03-25 22:15:43 -0700

    Finish adding basic regex functionality (this is a C++11 feature). This will simplify the parsing process.

--- parse.cc
+++ parse.cc
@@ -3,7 +3,6 @@
 #include <regex>
 #include <string>
 
-
 class TEnsdfRecord {
 
   public:
@@ -16,27 +15,49 @@ class TEnsdfRecord {
 };
 
 // Regular expressions for identifying ensdf record types
-std::string nuc_id = "";
-std::string primary_record = "";
-std::string continuation_record = "";
-std::string record_data = "";
-// g++ 4.8 doesn't yet support all C++11 features, including regex.
-// When 4.9+ has been out longer, you 
-//std::regex rx_primary_level_record(nuc_id + primary_record + " L " + record_data);
-std::regex rx_primary_level_record("^[a-zA-Z0-9]\\{5\\}[ 1] L .\\{71\\}$");
+std::string nuc_id = "^[[:alnum:] ]{5}";
+std::string primary_record = "[ 1]";
+std::string continuation_record = "[^ 1]";
+std::string record_data = ".{71}";
+
+std::regex rx_primary_level_record(nuc_id + primary_record + " L " + record_data);
 std::regex rx_continuation_level_record(nuc_id + continuation_record + " L " + record_data);
+std::regex rx_primary_gamma_record(nuc_id + primary_record + " G " + record_data);
+std::regex rx_continuation_gamma_record(nuc_id + continuation_record + " G " + record_data);
+
+std::string process_continuation_records(std::ifstream &file_in,
+  std::string &record, std::regex &rx_cont_record) {
+
+  std::string line;
+  while (!file_in.eof()) {
+    // Get the next line of the file
+    std::getline(file_in, line);
+ 
+    // Check to see if the next line is a continuation record 
+    if (std::regex_match(line, rx_cont_record)) {
+      // If it is, add the next line to the current
+      // record text.
+      record += "\n" + line;
+    }
+
+    // If a non-continuation-record line is found,
+    // stop reading from the file.
+    else return line; 
+  
+  }
+
+}
+
 
 
 int main() {
-
-  std::cout << "blah!";
 
   std::ifstream file_in("ensdf.040");
 
   std::string line; // String to store the current line
                     // of the ENSDF file during parsing
 
-  std::string temp_line;
+  std::string record;
 
   bool no_advance = false; // Flag that prevents advancing through
                            // the ENSDF file when a continuation record
@@ -51,58 +72,38 @@ int main() {
     std::string nuc_id = line.substr(0,5);
     if (nuc_id == " 40K ") {
 
-      // Characters that follow the NUCID entry
-      // on each line. These indicate which type
-      // of record is found on the line.
-      std::string rec_type_id = line.substr(5,4);
-
       // Level Record
       if (std::regex_match(line, rx_primary_level_record)) {
-        temp_line = line;
-        // Check for continuation records    
-        while (!file_in.eof() && !no_advance) {
-          // Get the next line of the file
-          std::getline(file_in, line);
-         
-          // Check to see if the next line is a continuation record 
-          if (std::regex_match(line, rx_continuation_level_record)) {
-            // If it is, add the next line to the current
-            // record text.
-            temp_line += "\n" + line;
-          }
-
-          else {
-            // If it isn't, set a flag so that the parsing
-            // loop will restart without advancing to the next
-            // line
-            no_advance = true;
-          }
+        record = line;
+        line = process_continuation_records(file_in, record, rx_continuation_level_record);
+        no_advance = true;
           
-        }
-
-        std::cout << "level record:" << std::endl << temp_line << std::endl;
-        
+        std::cout << "level record:" << std::endl << record << std::endl;
       }
 
       // Gamma Record
-      else if (rec_type_id.substr(1,3) == " G ") {
-        std::cout << "gamma record:" << std::endl << line << std::endl;
+      else if (std::regex_match(line, rx_primary_gamma_record)) {
+        record = line;
+        line = process_continuation_records(file_in, record, rx_continuation_gamma_record);
+        no_advance = true;
+
+        std::cout << "gamma record:" << std::endl << record << std::endl;
       }
      
-      // Parent Record
-      else if (rec_type_id.substr(0,3) == "  P") {
-        std::cout << "parent record:" << std::endl << line << std::endl;
-      }
+      //// Parent Record
+      //else if (rec_type_id.substr(0,3) == "  P") {
+      //  std::cout << "parent record:" << std::endl << line << std::endl;
+      //}
 
-      // Normalization Record
-      else if (rec_type_id.substr(0,3) == "  N") {
-        std::cout << "normalization record:" << std::endl << line << std::endl;
-      }
-      
-      // Product Normalization Record
-      else if (rec_type_id.substr(1,2) == "PN") {
-        std::cout << "product normalization record:" << std::endl << line << std::endl;
-      }
+      //// Normalization Record
+      //else if (rec_type_id.substr(0,3) == "  N") {
+      //  std::cout << "normalization record:" << std::endl << line << std::endl;
+      //}
+      //
+      //// Product Normalization Record
+      //else if (rec_type_id.substr(1,2) == "PN") {
+      //  std::cout << "product normalization record:" << std::endl << line << std::endl;
+      //}
 
     }
 
