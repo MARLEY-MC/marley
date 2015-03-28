c60f56f17a578923ca808e48b5c8592983252db8
Steven Gardiner <sjgardiner@ucdavis.edu>
2015-03-28 12:44:46 -0700

    Create decay scheme classes and populate them with parsed ENSDF gamma decay data

---
+++ ensdf_utils.hh
@@ -0,0 +1,83 @@
+#pragma once // Nonstandard but widely-supported (see http://en.wikipedia.org/wiki/Pragma_once)
+             // preprocessor directive that prevents this file from being included multiple times.
+             // Consider changing to an include guard (http://en.wikipedia.org/wiki/Include_guard)
+             // if desired by project collaborators.
+
+#include <string>
+
+namespace ensdf_utils {
+  
+  const std::string whitespace = " \f\n\r\t\v"; 
+
+  // This version of std::stod will return 0 if it encounters
+  // an empty string or an all-whitespace string.
+  inline double str_to_double(const std::string& s) {
+    size_t endpos = s.find_last_not_of(whitespace);
+    if (endpos == std::string::npos) {
+      return 0.0; // string was all whitespace
+    }
+    else {
+      return std::stod(s);
+    }
+  }
+
+  // These std::string trimming functions were taken from code
+  // presented here: http://www.cplusplus.com/faq/sequences/strings/trim/
+  // and here: http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+  // The first three (with the suffix copy) return a trimmed copy of
+  // the string without modifying the original.
+  inline std::string trim_right_copy(const std::string& s,
+    const std::string& delimiters = whitespace)
+  {
+    size_t endpos = s.find_last_not_of(delimiters);
+    return (endpos == std::string::npos) ? "" : s.substr(0, endpos + 1);
+  }
+  
+  inline std::string trim_left_copy(const std::string& s,
+    const std::string& delimiters = whitespace)
+  {
+    size_t startpos = s.find_first_not_of(delimiters);
+    return (startpos == std::string::npos) ? "" : s.substr(startpos);
+  }
+  
+  inline std::string trim_copy(const std::string& s,
+    const std::string& delimiters = whitespace)
+  {
+    return trim_left_copy(trim_right_copy(s, delimiters), delimiters);
+  }
+  
+  // The second three alter the original string, returning it
+  // after it has been trimmed. 
+  inline std::string& trim_right_inplace(std::string& s,
+    const std::string& delimiters = whitespace)
+  {
+    size_t endpos = s.find_last_not_of(delimiters);
+    if (endpos == std::string::npos) {
+      s.clear();
+    }
+    else {
+      s.erase(endpos + 1);
+    }
+    return s; 
+  }
+  
+  inline std::string& trim_left_inplace(std::string& s,
+    const std::string& delimiters = whitespace)
+  {
+    size_t startpos = s.find_first_not_of(delimiters);
+    if (startpos == std::string::npos) {
+      s.clear();
+    }
+    else {
+      s.erase(0, startpos);
+    }
+    return s; 
+  }
+  
+  inline std::string& trim_inplace(std::string& s,
+    const std::string& delimiters = whitespace)
+  {
+    return trim_left_inplace(trim_right_inplace(s,delimiters), delimiters);
+  }
+
+}
