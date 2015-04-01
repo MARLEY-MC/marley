09a69bb03be166e41d7f2c8de7ed14cd9f0dfe9f
Steven Gardiner <gardiner.steven@gmail.com>
2015-04-01 03:41:50 -0700

    Add function to print a latex table summarizing the contents of the decay scheme object

--- ensdf_utils.hh
+++ ensdf_utils.hh
@@ -7,6 +7,82 @@
 
 namespace ensdf_utils {
 
+  // Strings to use for latex table output of ENSDF data
+  std::string latex_table_1 = "\\documentclass[12pt]{article}\n"
+    "\n"
+    "\\usepackage{amsmath}\n"
+    "\\usepackage{booktabs}\n"
+    "\\usepackage[justification=justified,\n"
+    "width=\\textwidth,\n"
+    "labelformat=empty]{caption}\n"
+    "\\usepackage[top=1in, bottom=1in, left=0.25in, right=0.25in]{geometry}\n"
+    "\\usepackage{isotope}\n"
+    "\\usepackage{longtable}\n"
+    "\\usepackage{multirow}\n"
+    "\\usepackage{siunitx}\n"
+    "\n"
+    "\\newcommand{\\ExtraRowSpace}{1cm}\n"
+    "\n"
+    "\\begin{document}\n"
+    "\\begin{center}\n"
+    "\\begin{longtable}{\n"
+    "S[table-number-alignment = center,\n"
+    "  table-text-alignment = center]\n"
+    "c\n"
+    "S[table-number-alignment = center,\n"
+    "  table-text-alignment = center]\n"
+    "%  table-column-width = 2cm]\n"
+    "S[table-number-alignment = center,\n"
+    "  table-text-alignment = center]\n"
+    "S[table-number-alignment = center,\n"
+    "  table-text-alignment = center]\n"
+    "}\n"
+    "\\caption";
+
+  std::string latex_table_2 = "\\toprule\n"
+    "%{\\centering\\textbf{Level Energy (keV)}}\n"
+    "%& {\\centering\\textbf{Spin-Parity}}\n"
+    "%& {\\centering\\textbf{$\\boldsymbol{\\gamma}$ Energy (keV)}}\n"
+    "%& {\\centering\\textbf{$\\boldsymbol{\\gamma}$ RI}}\n"
+    "%& {\\centering\\textbf{Final Energy (keV)}} \\\\\n"
+    "{\\multirow{3}{2cm}{\\centering\\textbf{Level Energy (keV)}}}\n"
+    "& {\\multirow{3}{2cm}{\\centering\\textbf{Spin-Parity}}}\n"
+    "& {\\multirow{3}{2cm}{\\centering\\textbf{$\\boldsymbol{\\gamma}$ Energy (keV)}}}\n"
+    "& {\\multirow{3}{2cm}{\\centering\\textbf{$\\boldsymbol{\\gamma}$ RI}}}\n"
+    "& {\\multirow{3}{2.5cm}{\\centering\\textbf{Final Level Energy (keV)}}} \\\\\n"
+    "& & & & \\\\\n"
+    "& & & & \\\\\n"
+    "\\midrule\n"
+    "\\endfirsthead\n"
+    "\\caption[]";
+
+  std::string latex_table_3 = "\\toprule\n"
+    "%{\\centering\\textbf{Level Energy (keV)}}\n"
+    "%& {\\centering\\textbf{Spin-Parity}}\n"
+    "%& {\\centering\\textbf{$\\boldsymbol{\\gamma}$ Energy (keV)}}\n"
+    "%& {\\centering\\textbf{$\\boldsymbol{\\gamma}$ RI}}\n"
+    "%& {\\centering\\textbf{Final Energy (keV)}} \\\\\n"
+    "{\\multirow{3}{2cm}{\\centering\\textbf{Level Energy (keV)}}}\n"
+    "& {\\multirow{3}{2cm}{\\centering\\textbf{Spin-Parity}}}\n"
+    "& {\\multirow{3}{2cm}{\\centering\\textbf{$\\boldsymbol{\\gamma}$ Energy (keV)}}}\n"
+    "& {\\multirow{3}{2cm}{\\centering\\textbf{$\\boldsymbol{\\gamma}$ RI}}}\n"
+    "& {\\multirow{3}{2.5cm}{\\centering\\textbf{Final Level Energy (keV)}}} \\\\\n"
+    "& & & & \\\\\n"
+    "& & & & \\\\\n"
+    "\\midrule\n"
+    "\\noalign{\\vspace{-\\ExtraRowSpace}}\n"
+    "\\endhead\n"
+    "\\bottomrule \\multicolumn{4}{r}{\\textit{Continued on next page}} \\\\\n"
+    "\\endfoot\n"
+    "\\bottomrule\n"
+    "\\endlastfoot\n"
+    "% Table data\n"
+    "\n";
+
+  std::string latex_table_4 = "\\end{longtable}\n"
+    "\\end{center}\n"
+    "\\end{document}";
+
   // Random number generator that will be used when selecting gammas for
   // cascade simulations. Seed it using the system time
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -27,6 +103,14 @@ namespace ensdf_utils {
     else {
       return std::stod(s);
     }
+  }
+
+  // Function that creates a copy of a std::string object
+  // that has been converted to all lowercase
+  inline std::string to_lowercase(const std::string& s) {
+    std::string new_s = s;
+    std::transform(new_s.begin(), new_s.end(), new_s.begin(), ::tolower);
+    return new_s;
   }
 
   // These std::string trimming functions were taken from code
