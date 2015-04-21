ad8384205372c05965d4bab24e787c4eccf5a3fd
Steven Gardiner <gardiner.steven@gmail.com>
2015-04-21 16:04:56 -0700

    Split single source file into separate source and header files for each class. Add "Marley" prefix to all class names and to the utilities namespace. Enable all warnings in the Makefile.
    Former-commit-id: c01590ecc6d784808494bfb7c42a5c81e01562a7

---
+++ TMarleyDecayScheme.hh
@@ -0,0 +1,32 @@
+#pragma once
+#include <map>
+#include <regex>
+#include "TMarleyLevel.hh"
+
+class TMarleyDecayScheme {
+  public:
+    TMarleyDecayScheme(std::string nucid, std::string filename);
+    std::string get_nuc_id() const;
+    void set_nuc_id(std::string id);
+    void add_level(const TMarleyLevel level);
+    std::map<std::string, TMarleyLevel>* get_levels();
+    TMarleyLevel* get_level(std::string energy);
+    std::vector<TMarleyLevel*>* get_sorted_level_pointers();
+    void print_report(std::ostream& ostr = std::cout);
+    void do_cascade(std::string initial_energy);
+    void do_cascade(double initial_energy);
+    void do_cascade(TMarleyLevel* initial_level);
+    void print_latex_table(std::ostream& ostr = std::cout);
+
+  private:
+    std::string nuc_id;
+    std::string file_name;
+    std::map<std::string, TMarleyLevel> levels;
+    std::vector<TMarleyLevel*> pv_sorted_levels;
+    std::vector<double> sorted_level_energies;
+    static bool compare_level_energies(TMarleyLevel* first,
+      TMarleyLevel* second);
+    std::string process_continuation_records(std::ifstream &file_in,
+      std::string &record, std::regex &rx_cont_record) const;
+
+};
