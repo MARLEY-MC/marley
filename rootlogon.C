ff400936f7f74ebb8863b8d95640ef772cf5795d
Steven Gardiner <gardiner.steven@gmail.com>
2015-06-20 13:06:21 -0700

    Add scripts that will automatically load/unload TMarleyParticle and TMarleyEvent class dictionaries for ROOT when running it in the MARLEY source directory. These can be copied to an event analysis directory as needed since ROOT's default behavior is to search for them in the working directory every time it runs.
    Former-commit-id: 361350456e6e2801a2c890b8635135f11ff850a5

---
+++ rootlogon.C
@@ -0,0 +1,29 @@
+// Add C++11 support (currently non-default) to ACLiC
+void aclic_cpp11() {
+  TString o;
+  // Customize MakeSharedLib
+  o = TString(gSystem->GetMakeSharedLib());
+  o = o.ReplaceAll(" -c ", " -std=c++11 -c ");
+  gSystem->SetMakeSharedLib(o.Data());
+  // Customize MakeExe
+  o = TString(gSystem->GetMakeExe());
+  o = o.ReplaceAll(" -c ", " -std=c++11 -c ");
+  gSystem->SetMakeExe(o.Data());
+  // Customize interpreter's and compiler's flags
+  // gSystem->AddIncludePath("-D__WHATEVER_YOU_NEED__");
+}
+
+void load_marley_dict() {
+  // Instruct ACLiC to use C++11 compiler flags
+  aclic_cpp11();
+
+  // Load dictionaries for MARLEY classes that
+  // can be written to ROOT files
+  gROOT->ProcessLine(".L TMarleyParticle.cc+");
+  gROOT->ProcessLine(".L TMarleyEvent.cc+");
+}
+
+void rootlogon() {
+  std::cout << std::endl << "Creating temporary MARLEY class dictionaries ..." << std::endl;
+  load_marley_dict();
+}
