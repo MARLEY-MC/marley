38a934b5a0f4519ec0e44e1f557c3736a2392b13
Steven Gardiner <sjgardiner@ucdavis.edu>
2015-08-20 23:45:10 -0700

    Clean up ENSDF parsing test program

--- parse.cc
+++ parse.cc
@@ -9,11 +9,9 @@ int main() {
   std::vector<std::string> nuc_ids = { " 40AR",  " 40K ", " 40CA", " 40SC", " 40TI" };
   std::string filename = "ensdf.040";
 
-  for (std::vector<std::string>::iterator i = nuc_ids.begin();
-    i != nuc_ids.end(); ++i)
-  {
-    TMarleyDecayScheme ds = TMarleyDecayScheme((*i), filename); 
-    //ofs.open(marley_utils::trim_copy((*i)) + ".txt", std::ofstream::trunc);
+  for (auto &id : nuc_ids) {
+    TMarleyDecayScheme ds = TMarleyDecayScheme(id, filename);
+    //ofs.open(marley_utils::trim_copy(id) + ".txt", std::ofstream::trunc);
     ds.print_report();//(ofs);
     std::cout << std::endl;
     //ofs.close();
