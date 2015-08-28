a3a8f4bfc8a05e2f5537b9c063032421ccbda45f
Steven Gardiner <gardiner.steven@gmail.com>
2015-08-28 00:28:17 -0700

    Add class implementing the back-shifted Fermi gas level density model.
    Former-commit-id: c8173c0bff24f293d4b2286606f8c43a213e0f3a

---
+++ TMarleyBackshiftedFermiGasModel.hh
@@ -0,0 +1,18 @@
+// TODO: change this from an all-static class so that one can create objects
+// with local fit parameters for a specific nuclide
+class TMarleyBackshiftedFermiGasModel {
+  public:
+    static double level_density(int Z, int A, double Ex); // rho(Ex)
+    // rho(Ex, J, Pi) with the assumption of parity equipartition
+    static double level_density(int Z, int A, double Ex, int two_J);
+  private:
+    // Version of the level density function used internally
+    static double level_density(int Z, int A, double Ex, double& sigma);
+
+    // Parameters from global level density fit performed in A. J. Koning,
+    // et al., Nucl. Phys. A810 (2008) pp. 13-76.
+    static constexpr double alpha = 0.0722396;
+    static constexpr double beta = 0.195267;
+    static constexpr double gamma_1 = 0.410289;
+    static constexpr double delta_global = 0.173015;
+};
