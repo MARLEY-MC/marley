827b0658298c63263d1ee62dad22c730b76000ca
Steven Gardiner <gardiner.steven@gmail.com>
2015-11-20 04:21:36 -0800

    Tweak the integrator class so that quadrature weights are precomputed during construction. Add a test program for the integrator.
    Former-commit-id: da3feea6427df3ad914325deb567ba9dde18d953

--- TMarleyIntegrator.cc
+++ TMarleyIntegrator.cc
@@ -8,35 +8,28 @@
 // Clenshaw-Curtis quadrature at 2N sampling points.  (see
 // http://en.wikipedia.org/wiki/Clenshaw-Curtis_quadrature)
 double TMarleyIntegrator::num_integrate(const std::function<double(double)> &f,
-  double a, double b, int N)
+  double a, double b)
 {
-  double twoN = 2*N;
+  int two_N = 2*N;
   double A = (b - a)/2;
   double B = (b + a)/2;
-  double C = (f(b) + f(a))/twoN;
+  double C = (f(b) + f(a))/(two_N);
   double D = f(B)/N;
 
-  std::vector<double> eval_points;
-  double npi = 0;
-  for (int n = 1; n < N; ++n) {
-    npi += marley_utils::pi;
-    eval_points.push_back(A*std::cos(npi/twoN));
-  }
+  double integral = 0.;
+  int sign = 1;
+  for(size_t k = 0; k <= N; sign *= -1, ++k) {
 
-  double integral = 0;
-
-  for(int k = 0; k <= N; ++k) {
-
-    npi = 0;
-    int sign = 1;
-    if (k % 2 == 1) sign = -1;
     double term = C + sign*D;
-    for (int n = 1; n < N; ++n) {
-      npi += marley_utils::pi;
-      double epoint = eval_points[n-1];
+    for (size_t n = 1; n < N; ++n) {
+      double epoint = A * weights[n];
 
-      term += (f(B + epoint) + f(B - epoint))
-        * std::cos(npi*k/N)/N;
+      double nk_weight;
+      size_t nk_mod = n*k % two_N;
+      if (nk_mod >= N) nk_weight = -weights[2*(nk_mod % N)];
+      else nk_weight = weights[2*nk_mod];
+      
+      term += (f(B + epoint) + f(B - epoint)) * nk_weight/N;
     }
 
     if (k != 0 && k != N) term *= 2;
