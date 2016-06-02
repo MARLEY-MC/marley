357b84e200c0c7e59d8709815da05d0ee7617ced
Steven Gardiner <sjgardiner@ucdavis.edu>
2016-06-02 09:31:33 -0700

    Add configuration file option (keywords are 'th1' or 'roothist') to load a ROOT TH1 object from a file and use it as the energy spectrum for a GridNeutrinoSource
    Former-commit-id: 576e16fdb9f15c498ed7ff116d96f1b8252fc11f

--- src/ConfigFile.cc
+++ src/ConfigFile.cc
@@ -2,6 +2,12 @@
 #include "ConfigFile.hh"
 #include "InterpolationGrid.hh"
 
+#ifdef USE_ROOT
+#include "TAxis.h"
+#include "TFile.h"
+#include "TH1.h"
+#endif
+
 // Define type abbreviations and constant regular expressions that are only
 // visible within this file
 namespace {
@@ -296,6 +302,87 @@ marley::ConfigFile::ConfigFile(std::string file_name)
           + std::to_string(line_num) + " of the configuration file "
           + filename);
       }
+
+      #ifdef USE_ROOT
+      // 1D histogram from a ROOT TFile
+      if (arg == "th1" || arg == "roothist") {
+
+        // Get the file name and namecycle for the histogram
+        // from the configuration file
+        std::string source_tfile_name, namecycle;
+        next_word_from_line(source_tfile_name, true, false);
+        next_word_from_line(namecycle, true, false);
+
+        // Attempt to open the TFile and complain if something goes wrong
+        std::unique_ptr<TFile> source_tfile(TFile::Open(
+          source_tfile_name.c_str(), "read"));
+
+        if (source_tfile) {
+
+          // Attempt to read in the histogram (TH1 object) from the file
+          // using the given namecycle
+          std::unique_ptr<TH1> source_th1(dynamic_cast<TH1*>(
+            source_tfile->Get(namecycle.c_str())));
+
+          if (source_th1) {
+
+            // The histogram was successfully recovered from the
+            // TFile, so read in the (energy low edge, bin weight)
+            // ordered pairs. Keep the overflow bin so that we can
+            // use its left edge as the maximum energy value. Skip
+            // the unneeded underflow bin.
+            const TAxis* x_axis = source_th1->GetXaxis();
+            if (!x_axis) throw marley::Error(std::string("Error finding x-axis")
+              + " of ROOT histogram (TH1 object) with namecycle '" + namecycle
+              + "' from the ROOT file '" + source_tfile_name + "' given on"
+              + " line " + std::to_string(line_num)
+              + " of the configuration file " + filename);
+            // include the overflow bin but not the underflow bin
+            size_t nbins = x_axis->GetNbins() + 1;
+            std::vector<double> Es(nbins);
+            std::vector<double> PDFs(nbins);
+
+            for (size_t b = 1; b <= nbins; ++b) { // underflow bin is bin 0
+              Es.at(b - 1) = x_axis->GetBinLowEdge(b);
+              // the content of the overflow bin is reset to zero below
+              PDFs.at(b - 1) = source_th1->GetBinContent(b);
+            }
+
+            // Convert the bin weights to probability densities (used
+            // by our GridNeutrinoSource object) by dividing each bin
+            // weight by the corresponding bin width.
+            for (size_t c = 0; c < nbins - 1; ++c) {
+              PDFs.at(c) /= Es.at(c + 1) - Es.at(c);
+            }
+
+            // Assign zero probability density to the overflow bin's
+            // left edge. This ensures that neutrino energies will be
+            // sampled on the half-open interval [Elow, Ehigh), where
+            // Elow is the left edge of the first bin and Ehigh is the
+            // left edge of the overflow bin.
+            PDFs.back() = 0.;
+
+            // Now that we've processed grid points, create the grid neutrino
+            // source
+            sources.push_back(std::make_unique<marley::GridNeutrinoSource>(
+              Es, PDFs, neutrino_pid, weight, InterpMethod::Constant));
+          }
+          else { // couldn't read the histogram
+            throw marley::Error(std::string("Could not recover")
+              + " ROOT histogram (TH1 object) with namecycle '" + namecycle
+              + "' from the ROOT file '" + source_tfile_name + "' given on"
+              + " line " + std::to_string(line_num)
+              + " of the configuration file " + filename);
+          }
+        }
+        else { // couldn't open ROOT file
+          throw marley::Error(std::string("Could not open")
+            + " ROOT file '" + source_tfile_name
+            + "' given on line " + std::to_string(line_num)
+            + " of the configuration file " + filename);
+        }
+      }
+      #endif
 
       else if (arg == "dar" || arg == "decay-at-rest") {
         sources.push_back(std::make_unique<marley::DecayAtRestNeutrinoSource>(
