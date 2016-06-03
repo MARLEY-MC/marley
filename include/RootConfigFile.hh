9380262dbc83b143c09341f4acfe95e4f7287290
Steven Gardiner <sjgardiner@ucdavis.edu>
2016-06-02 22:32:27 -0700

    Create template member function for the RootConfigFile class to retrieve an arbitrary object from a ROOT TFile.

--- include/RootConfigFile.hh
+++ include/RootConfigFile.hh
@@ -16,6 +16,10 @@ namespace marley {
     protected:
       virtual bool process_extra_source_types(const std::string& type,
         int neutrino_pid, double weight) override;
+
+      // Helper function for loading objects from a ROOT TFile
+      template<typename T> T* get_root_object(const std::string& tfile_name,
+        const std::string& namecycle);
   };
 
 }
