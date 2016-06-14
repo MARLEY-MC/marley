9bdc72c7926e7bf5fa790350c7814ae6fcfe371f
Steven Gardiner <sjgardiner@ucdavis.edu>
2016-06-14 16:00:45 -0700

    Continue adding Doxygen documentation, including a Doxyfile.
    Former-commit-id: 504ee2d68245178b8824fc664441838f3ae7829b

--- include/Fragment.hh
+++ include/Fragment.hh
@@ -1,51 +1,74 @@
 #pragma once
-
 #include "MassTable.hh"
 #include "Parity.hh"
 
 namespace marley {
 
-  // Stores data about a nuclear fragment
+  /// @brief Simple container for storing reference data about
+  /// each of the nuclear fragments considered by MARLEY's
+  /// compound nucleus de-excitation models
   class Fragment {
+
     public:
-      inline Fragment(int p_id, int twoS, marley::Parity pi) {
-        pid = p_id;
-        two_s = twoS;
-        parity = pi;
-        Z = marley::MassTable::get_particle_Z(p_id);
-        A = marley::MassTable::get_particle_A(p_id);
-      }
 
-      inline int get_pid() const {
-        return pid;
-      }
+      /// @param pid PDG particle ID
+      /// @param twoS Two times the fragment's spin
+      /// @param pi The fragment's parity
+      inline Fragment(int pid, int twoS, marley::Parity pi);
 
-      inline int get_two_s() const {
-        return two_s;
-      }
+      /// Get the PDG particle ID for this fragment
+      inline int get_pid() const;
 
-      inline marley::Parity get_parity() const {
-        return parity;
-      }
+      /// Get two times the spin of this fragment
+      inline int get_two_s() const;
 
-      inline int get_Z() const {
-        return Z;
-      }
+      /// Get the parity of this fragment
+      inline marley::Parity get_parity() const;
 
-      inline int get_A() const {
-        return A;
-      }
+      /// Get the atomic number of this fragment
+      inline int get_Z() const;
 
-      inline double get_mass() const {
-        return marley::MassTable::get_particle_mass(pid);
-      }
+      /// Get the mass number of this fragment
+      inline int get_A() const;
+
+      /// Get the mass (in MeV) of this fragment
+      inline double get_mass() const;
 
     private:
-      int pid; // Particle ID for this fragment
-      int two_s; // Two times the spin of the fragment (allows for
-                 // half-integer spins)
-      marley::Parity parity; // Intrinsic parity of the fragment
-      int Z, A; // Atomic and mass numbers (extracted from pid upon construction)
+      int pid_; ///< PDG particle ID
+
+      /// Two times the fragment spin (allows for half-integer spins)
+      int two_s_;
+
+      marley::Parity parity_; ///< intrinsic parity
+      int Z_; ///< atomic number
+      int A_; ///< mass number
   };
+
+// Inline function definitions
+inline Fragment::Fragment(int pid, int twoS, marley::Parity pi)
+  : pid_(pid), two_s_(twoS), parity_(pi)
+{
+  Z_ = marley::MassTable::get_particle_Z(pid);
+  A_ = marley::MassTable::get_particle_A(pid);
+}
+
+inline int Fragment::get_pid() const
+  { return pid_; }
+
+inline int Fragment::get_two_s() const
+  { return two_s_; }
+
+inline marley::Parity Fragment::get_parity() const
+  { return parity_; }
+
+inline int Fragment::get_Z() const
+  { return Z_; }
+
+inline int Fragment::get_A() const
+  { return A_; }
+
+inline double Fragment::get_mass() const
+  { return marley::MassTable::get_particle_mass(pid_); }
 
 }
