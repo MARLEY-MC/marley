b32c4ff98f1020bfea804c34309874f364202b90
Steven Gardiner <sjgardiner@ucdavis.edu>
2016-06-21 01:14:15 -0700

    Optimize the Integrator class.
    Former-commit-id: 8257ea07356d8f8a1256f1edd5d4f3ed3c0d4373

--- include/Integrator.hh
+++ include/Integrator.hh
@@ -13,16 +13,16 @@ namespace marley {
     public:
 
       /// @brief Create a Clenshaw-Curtis quadrature integrator that uses
-      /// 2n sampling points
-      /// @param n half the number of sampling points to use
-      Integrator(size_t n = N_DEFAULT_);
+      /// 2*num sampling points
+      /// @param num half the number of sampling points to use
+      Integrator(size_t num = N_DEFAULT_);
 
       /// @brief Numerically integrate an arbitrary 1D function
       /// @details Numerically integrate a std::function<double(double)> over the
       /// interval [a,b] using <a
       /// href="http://en.wikipedia.org/wiki/Clenshaw-Curtis_quadrature">
       /// Clenshaw-Curtis quadrature</a> at 2N_ sampling points.
-      double num_integrate(const std::function<double(double)> &f,
+      double num_integrate(const std::function<double(double)>& f,
         double a, double b) const;
 
     private:
@@ -30,8 +30,11 @@ namespace marley {
       /// @brief use 2N_ sampling points to perform numerical integration
       size_t N_;
 
-      // @brief precomputed weights to use to speed up integration
+      /// @brief precomputed weights to use to speed up integration
       std::vector<double> weights_;
+
+      /// @brief storage for function evaluation point offsets
+      std::vector<double> offsets_;
 
       static constexpr size_t N_DEFAULT_ = 20; ///< default value of N_
   };
--- src/Integrator.cc
+++ src/Integrator.cc
@@ -3,50 +3,46 @@
 #include "marley_utils.hh"
 #include "Integrator.hh"
 
-marley::Integrator::Integrator(size_t n) : N_(n) {
-  /// @todo add error check for when n == 0
-  /// @todo add error check for when n is ridiculously large
+marley::Integrator::Integrator(size_t num) : N_(num), weights_(num + 1, 0.),
+  offsets_(num - 1)
+{
+  /// @todo add error check for when num == 0 or too small
+  /// @todo add error check for when num is ridiculously large
 
-  // Precompute the 2N - 1 weights for speed
-  size_t two_N = 2*N_;
-  double npi_over_two = -marley_utils::half_pi;
-  for (size_t k = 0; k < two_N; ++k) {
-    npi_over_two += marley_utils::half_pi;
-    weights_.push_back(std::cos(npi_over_two / N_));
+  // Precompute the N_ - 1 offsets for speed
+  double arg = 0.;
+  for (size_t n = 0; n < N_ - 1; ++n) {
+    arg += marley_utils::half_pi / N_;
+    offsets_[n] = std::cos(arg);
+  }
+
+  // Also precompute the N_ + 1 weights
+  for (size_t n = 0; n <= N_; ++n) {
+    for (size_t k = 0; k <= N_; ++k) {
+      double weight_piece = std::cos(n * k * marley_utils::pi / N_)
+        / (1. - std::pow(2*k, 2));
+      if (k != 0 && k != N_) weight_piece *= 2.;
+      weights_[n] += weight_piece;
+    }
+    weights_[n] /= N_;
   }
 }
 
 double marley::Integrator::num_integrate(const std::function<double(double)>& f,
   double a, double b) const
 {
-  size_t two_N = 2*N_;
-  double A = (b - a)/2;
-  double B = (b + a)/2;
-  double C = (f(b) + f(a))/(two_N);
-  double D = f(B)/N_;
+  double A = (b - a) / 2.;
+  double B = (b + a) / 2.;
+  double C = (f(a) + f(b)) / 2.;
 
-  double integral = 0.;
-  int sign = 1;
-  for(size_t k = 0; k <= N_; sign *= -1, ++k) {
+  double integral = weights_[0] * C; // n = 0 term
+  integral += weights_[N_] * f(B); // n = N_ term
 
-    double term = C + sign*D;
-    for (size_t n = 1; n < N_; ++n) {
-      double epoint = A * weights_[n];
-
-      double nk_weight;
-      size_t nk_mod = n*k % two_N;
-      if (nk_mod >= N_) nk_weight = -weights_[2*(nk_mod % N_)];
-      else nk_weight = weights_[2*nk_mod];
-
-      term += (f(B + epoint) + f(B - epoint)) * nk_weight/N_;
-    }
-
-    if (k != 0 && k != N_) term *= 2;
-
-    term /= 1 - 4*std::pow(k, 2);
-
-    integral += term;
+  // n = 1 to n = N_ - 1 terms
+  for (size_t n = 1; n < N_; ++n) {
+    double epoint = A * offsets_[n - 1];
+    integral += weights_[n] * (f(B + epoint) + f(B - epoint));
   }
 
-  return A*integral;
+  return A * integral;
 }
