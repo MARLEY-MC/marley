d7c99413afa98a292a9dbb6fe5348249832375b0
Steven Gardiner <sjgardiner@ucdavis.edu>
2016-07-02 23:09:26 -0700

    Add Doxygen documentation for the Gamma class, and do some minor code cleanup.
    Former-commit-id: 69dced7b7ad92a44504aac7733192f2be01caf24

--- src/Gamma.cc
+++ src/Gamma.cc
@@ -1,32 +1,8 @@
 #include "Gamma.hh"
 
-marley::Gamma::Gamma(double energy, double ri, marley::Level* start_level) {
-  fEnergy = energy;
-  fRI = ri;
-  pStartLevel = start_level;
-  pEndLevel = nullptr;
-}
-
-void marley::Gamma::set_start_level(marley::Level* start_level) {
-  pStartLevel = start_level;
-}
-
-void marley::Gamma::set_end_level(marley::Level* end_level) {
-  pEndLevel = end_level;
-}
-
-marley::Level* marley::Gamma::get_start_level() const {
-  return pStartLevel;
-}
-
-marley::Level* marley::Gamma::get_end_level() const {
-  return pEndLevel;
-}
-
-double marley::Gamma::get_energy() const {
-  return fEnergy;
-}
-
-double marley::Gamma::get_ri() const {
-  return fRI;
+marley::Gamma::Gamma(double energy, double rel_intensity,
+  marley::Level* start_lev, marley::Level* end_lev) : energy_(energy),
+  relative_intensity_(rel_intensity), start_level_(start_lev),
+  end_level_(end_lev)
+{
 }
