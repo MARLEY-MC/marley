62765ddecba8696efec8d4477f9532c2f3dcc788
Steven Gardiner <gardiner@fnal.gov>
2018-10-29 17:26:58 -0500

    Explicitly request dictionaries for marley::Particle and marley::Event from ROOT.

--- include/marley/marley_linkdef.hh
+++ include/marley/marley_linkdef.hh
@@ -3,11 +3,11 @@
 #ifdef __MAKECINT__
 #pragma link C++ nestedclasses;
 #pragma link C++ nestedtypedefs;
-#pragma link C++ defined_in "marley/Event.hh";
-#pragma link C++ defined_in "marley/Particle.hh";
+#pragma link C++ class marley::Particle+;
 #pragma link C++ class std::vector<marley::Particle*>+;
 #pragma link C++ class std::vector<marley::Particle>::*+;
 #pragma link C++ operators std::vector<marley::Particle>::iterator;
 #pragma link C++ operators std::vector<marley::Particle>::const_iterator;
 #pragma link C++ operators std::vector<marley::Particle>::reverse_iterator;
+#pragma link C++ class marley::Event+;
 #endif
