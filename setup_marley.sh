40a43fa6c8b76cccbc076be43d09ecf256e67383
Steven Gardiner <gardiner@fnal.gov>
2020-01-03 20:16:38 -0600

    Small changes to the setup script and Doxyfile

--- setup_marley.sh
+++ setup_marley.sh
@@ -23,6 +23,6 @@ fi
 # For using MARLEY classes in ROOT
 export ROOT_INCLUDE_PATH=${THIS_DIRECTORY}/include
 
-# For building against MARLEY
+# For building external software against MARLEY
 export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${THIS_DIRECTORY}/include
 export LIBRARY_PATH=${LIBRARY_PATH}:${THIS_DIRECTORY}/build
