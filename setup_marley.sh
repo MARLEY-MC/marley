e6388d838cdb5069cf9efca0438a6ab0782c5cd5
Steven Gardiner <gardiner@fnal.gov>
2020-09-01 14:44:12 -0500

    Add handling for an undefined BASH_SOURCE variable to setup_marley.sh. This sometimes occurs when running zsh on recent versions of macOS.

--- setup_marley.sh
+++ setup_marley.sh
@@ -3,8 +3,9 @@
 # Finds the directory in which this script is located. This method isn't
 # foolproof. See https://stackoverflow.com/a/246128/4081973 if you need
 # something more robust for edge cases (e.g., you're calling the script using
-# symlinks).
-THIS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# symlinks). To make this technique work in zsh as well as bash, use the
+# approach described here: https://stackoverflow.com/a/54755784
+THIS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )"
 
 # The root MARLEY folder
 export MARLEY=${THIS_DIRECTORY}
