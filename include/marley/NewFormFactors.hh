cfdf0269f0028600032d004dc096d91e4e20f665
Steven Gardiner <gardiner@fnal.gov>
2024-11-24 12:42:35 -0600

    Add new code to calculate form factors. In a future commit, NewFormFactors will replace the current FormFactors class.

---
+++ include/marley/NewFormFactors.hh
@@ -0,0 +1,232 @@
+/// @file
+/// @copyright Copyright (C) 2016-2024 Steven Gardiner
+/// @license GNU General Public License, version 3
+//
+// This file is part of MARLEY (Model of Argon Reaction Low Energy Yields)
+//
+// MARLEY is free software: you can redistribute it and/or modify it under the
+// terms of version 3 of the GNU General Public License as published by the
+// Free Software Foundation.
+//
+// For the full text of the license please see COPYING or
+// visit http://opensource.org/licenses/GPL-3.0
+//
+// Please respect the MCnet academic usage guidelines. See GUIDELINES
+// or visit https://www.montecarlonet.org/GUIDELINES for details.
+
+#pragma once
+
+// Standard library includes
+#include <cmath>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace marley {
+
+  class SachsFormFactors {
+    public:
+
+      SachsFormFactors() {}
+
+      /// Proton electric form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      virtual double GEp( double Q2 ) = 0;
+
+      /// Neutron electric form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      virtual double GEn( double Q2 ) = 0;
+
+      /// Proton magnetic form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      virtual double GMp( double Q2 ) = 0;
+
+      /// Neutron magnetic form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      virtual double GMn( double Q2 ) = 0;
+
+      /// Helper function that computes @f$ \tau = \frac{ Q^{2} }{ 4 m_N^{2} }
+      /// @f$
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      virtual double tau( double Q2 );
+  };
+
+  class DipoleSachsFormFactors : public SachsFormFactors {
+
+    public:
+
+      /// @param Mv Vector mass parameter (MeV)
+      DipoleSachsFormFactors( double Mv ) : Mv_( Mv ) {}
+
+      virtual double GEp( double Q2 ) override final;
+      inline virtual double GEn( double /*Q2*/ ) override final { return 0.; }
+      virtual double GMp( double Q2 ) override final;
+      virtual double GMn( double Q2 ) override final;
+
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      inline double dipole( double Q2 )
+        { return 1.0 / std::pow( 1.0 + Q2 / Mv_ / Mv_, 2 ); }
+
+    protected:
+
+      /// Vector mass parameter (MeV)
+      double Mv_;
+  };
+
+
+  class BBBA05SachsFormFactors : public SachsFormFactors {
+
+    public:
+
+      BBBA05SachsFormFactors() {}
+
+      virtual double GEp( double Q2 ) override final;
+      virtual double GEn( double Q2 ) override final;
+      virtual double GMp( double Q2 ) override final;
+      virtual double GMn( double Q2 ) override final;
+
+    protected:
+
+      /// @brief Helper function for the form factor calculation @details
+      /// Implements Eq. (6) from Nucl. Phys. B Proc. Suppl. 159, 127 (2006)
+      /// https://doi.org/10.1016/j.nuclphysbps.2006.08.028
+      /// @param tau Dimensionless negative square of the four-momentum
+      /// transfer @f$ \tau = \frac{ Q^{2} }{ 4 m_N^{2} } @f$
+      /// @param a_coeffs Coefficients @f$ a_k @f$ of polynomial involving @f$
+      /// \tau^k @f$ in the numerator
+      /// @param b_coeffs Coefficients @f$ b_k @f$ of polynomial involving @f$
+      /// \tau^k @f$ in the denominator
+      double bbba05_G( double tau, const std::vector< double >& a_coeffs,
+        const std::vector< double >& b_coeffs );
+  };
+
+  class AxialFormFactors {
+    public:
+
+      AxialFormFactors() {}
+
+      /// Axial-vector form factor
+      virtual double FA( double Q2 ) = 0;
+
+      /// Pseudoscalar form factor
+      virtual double FP( double Q2 ) = 0;
+  };
+
+  class DipoleAxialFormFactors : public AxialFormFactors {
+    public:
+
+      // @param gA Axial-vector coupling constant (dimensionless)
+      // @param Ma Axial mass parameter (MeV)
+      DipoleAxialFormFactors( double gA, double Ma ) : gA_( gA ), Ma_( Ma ) {}
+
+      inline double FA( double Q2 ) override final
+        { return -gA_ / std::pow( 1.0 + Q2 / Ma_ / Ma_, 2 ); }
+
+      double FP( double Q2 ) override final;
+
+    protected:
+
+      /// Axial-vector coupling constant of the nucleon (dimensionless)
+      double gA_;
+
+      /// Axial mass parameter (MeV)
+      double Ma_;
+  };
+
+  class NewFormFactors {
+    public:
+
+      NewFormFactors( const std::string& sachs_ff_model,
+        const std::string& axial_ff_model );
+
+      /// Charged-current vector form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      inline double F1( double Q2 ) const
+        { return this->F1p( Q2 ) - this->F1n( Q2 ); }
+
+      /// Charged-current weak magnetic form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      inline double F2( double Q2 ) const
+        { return this->F2p( Q2 ) - this->F2n( Q2 ); }
+
+      /// Proton vector form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      double F1p( double Q2 ) const;
+
+      /// Neutron vector form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      double F1n( double Q2 ) const;
+
+      /// Proton weak magnetic form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      double F2p( double Q2 ) const;
+
+      /// Neutron weak magnetic form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      double F2n( double Q2 ) const;
+
+      /// Charged-current axial-vector form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      inline double FA( double Q2 ) const { return axial_ff_->FA( Q2 ); }
+
+      /// Charged-current pseudoscalar form factor
+      /// @param Q2 Negative square @f$ Q^{2} @f$ of the four-momentum transfer
+      /// (MeV<sup> 2</sup>)
+      inline double FP( double Q2 ) const { return axial_ff_->FP( Q2 ); }
+
+      /// Get const access to the owned Sachs form factors
+      inline const SachsFormFactors* sachs_ff() const
+        { return sachs_ff_.get(); }
+
+      /// Get const access to the owned axial form factors
+      inline const AxialFormFactors* axial_ff() const
+        { return axial_ff_.get(); }
+
+    protected:
+
+      std::shared_ptr< SachsFormFactors > sachs_ff_;
+      std::shared_ptr< AxialFormFactors > axial_ff_;
+  };
+
+  /// Computes Sachs form factors in the limit @f$ Q^{2} \to 0 @f$
+  class TrivialSachsFormFactors : public SachsFormFactors {
+
+    public:
+
+      TrivialSachsFormFactors() {}
+
+      virtual double GEp( double Q2 ) override final;
+      virtual double GEn( double Q2 ) override final;
+      virtual double GMp( double Q2 ) override final;
+      virtual double GMn( double Q2 ) override final;
+
+      /// Neglect the @f$ Q^{2} @f$ dependence of the form factors entirely
+      inline virtual double tau( double /*Q2*/ ) override final { return 0.; }
+  };
+
+  /// Computes axial form factors in the limit @f$ Q^{2} \to 0 @f$
+  class TrivialAxialFormFactors : public AxialFormFactors {
+
+    public:
+
+      TrivialAxialFormFactors() {}
+
+      double FA( double Q2 ) override final;
+      double FP( double Q2 ) override final;
+
+  };
+
+} // marley namespace
---
+++ src/NewFormFactors.cc
@@ -0,0 +1,173 @@
+/// @file
+/// @copyright Copyright (C) 2016-2024 Steven Gardiner
+/// @license GNU General Public License, version 3
+//
+// This file is part of MARLEY (Model of Argon Reaction Low Energy Yields)
+//
+// MARLEY is free software: you can redistribute it and/or modify it under the
+// terms of version 3 of the GNU General Public License as published by the
+// Free Software Foundation.
+//
+// For the full text of the license please see COPYING or
+// visit http://opensource.org/licenses/GPL-3.0
+//
+// Please respect the MCnet academic usage guidelines. See GUIDELINES
+// or visit https://www.montecarlonet.org/GUIDELINES for details.
+
+// MARLEY includes
+#include "marley/Error.hh"
+#include "marley/NewFormFactors.hh"
+#include "marley/marley_utils.hh"
+
+double marley::DipoleSachsFormFactors::GEp( double Q2 ) {
+  return marley_utils::g_V * this->dipole( Q2 );
+}
+
+double marley::DipoleSachsFormFactors::GMp( double Q2 ) {
+  return marley_utils::mu_p * this->dipole( Q2 );
+}
+
+double marley::DipoleSachsFormFactors::GMn( double Q2 ) {
+  return marley_utils::mu_n * this->dipole( Q2 );
+}
+
+// Implements Eq. (6) from Nucl. Phys. B Proc. Suppl. 159, 127 (2006)
+// https://doi.org/10.1016/j.nuclphysbps.2006.08.028
+double marley::BBBA05SachsFormFactors::bbba05_G( double tau,
+  const std::vector< double >& a_coeffs,
+  const std::vector< double >& b_coeffs )
+{
+  double numer = 0.;
+  double denom = 1.;
+
+  double tau_pow = 1.;
+  for ( const auto& a : a_coeffs ) {
+    numer += a * tau_pow;
+    tau_pow *= tau;
+  }
+
+  tau_pow = tau;
+  for ( const auto& b : b_coeffs ) {
+    denom += b * tau_pow;
+    tau_pow *= tau;
+  }
+
+  double ff_G = numer / denom;
+  return ff_G;
+}
+
+double marley::BBBA05SachsFormFactors::GEp( double Q2 ) {
+  double tau = this->tau( Q2 );
+  // TODO: remove hard-coding here and make the parameters configurable
+  return marley_utils::g_V * this->bbba05_G( tau, { 1., -0.0578 },
+    { 11.1, 13.6, 33. } );
+}
+
+double marley::BBBA05SachsFormFactors::GMp( double Q2 ) {
+  double tau = this->tau( Q2 );
+  // TODO: remove hard-coding here and make the parameters configurable
+  return marley_utils::mu_p * this->bbba05_G( tau, { 1., 0.150 },
+    { 11.1, 19.6, 7.54 } );
+}
+
+double marley::BBBA05SachsFormFactors::GEn( double Q2 ) {
+  double tau = this->tau( Q2 );
+  // TODO: remove hard-coding here and make the parameters configurable
+  return this->bbba05_G( tau, { 0., 1.25, 1.30 },
+    { -9.86, 305., -758., 802. } );
+}
+
+double marley::BBBA05SachsFormFactors::GMn( double Q2 ) {
+  double tau = this->tau( Q2 );
+  // TODO: remove hard-coding here and make the parameters configurable
+  return marley_utils::mu_n * this->bbba05_G( tau, { 1., 1.81 },
+    { 14.1, 20.7, 68.7 } );
+}
+
+double marley::TrivialSachsFormFactors::GEp( double /*Q2*/ )
+  { return marley_utils::g_V; }
+
+double marley::TrivialSachsFormFactors::GEn( double /*Q2*/ ) { return 0.; }
+
+double marley::TrivialSachsFormFactors::GMp( double /*Q2*/ )
+  { return marley_utils::mu_p; }
+
+double marley::TrivialSachsFormFactors::GMn( double /*Q2*/ )
+  { return marley_utils::mu_n; }
+
+double marley::TrivialAxialFormFactors::FA( double /*Q2*/ )
+  { return -marley_utils::g_A; }
+
+double marley::TrivialAxialFormFactors::FP( double /*Q2*/ ) {
+  return -2. * marley_utils::g_A * marley_utils::m_nucleon
+    / marley_utils::m_pion / marley_utils::m_pion;
+}
+
+double marley::NewFormFactors::F1p( double Q2 ) const {
+  double tau = sachs_ff_->tau( Q2 );
+  double GEp = sachs_ff_->GEp( Q2 );
+  double GMp = sachs_ff_->GMp( Q2 );
+  double F1p = ( GEp + tau*GMp ) / ( 1. + tau );
+  return F1p;
+}
+
+double marley::NewFormFactors::F1n( double Q2 ) const {
+  double tau = sachs_ff_->tau( Q2 );
+  double GEn = sachs_ff_->GEn( Q2 );
+  double GMn = sachs_ff_->GMn( Q2 );
+  double F1n = ( GEn + tau*GMn ) / ( 1. + tau );
+  return F1n;
+}
+
+double marley::NewFormFactors::F2p( double Q2 ) const {
+  double tau = sachs_ff_->tau( Q2 );
+  double GEp = sachs_ff_->GEp( Q2 );
+  double GMp = sachs_ff_->GMp( Q2 );
+  double F2p = ( GMp - GEp ) / ( 1. + tau ) / 2. / marley_utils::m_nucleon;
+  return F2p;
+}
+
+double marley::NewFormFactors::F2n( double Q2 ) const {
+  double tau = sachs_ff_->tau( Q2 );
+  double GEn = sachs_ff_->GEn( Q2 );
+  double GMn = sachs_ff_->GMn( Q2 );
+  double F2n = ( GMn - GEn ) / ( 1. + tau ) / 2. / marley_utils::m_nucleon;
+  return F2n;
+}
+
+marley::NewFormFactors::NewFormFactors( const std::string& sachs_ff_model,
+  const std::string& axial_ff_model )
+{
+  // Choose the model to use for the Sachs form factors
+  if ( sachs_ff_model == "trivial" ) {
+    sachs_ff_ = std::make_shared< TrivialSachsFormFactors >();
+  }
+  else if ( sachs_ff_model == "dipole" ) {
+    sachs_ff_ = std::make_shared< DipoleSachsFormFactors >( marley_utils::M_V );
+  }
+  else if ( sachs_ff_model == "bbba05" ) {
+    sachs_ff_ = std::make_shared< BBBA05SachsFormFactors >();
+  }
+  else throw marley::Error( "Unrecognized Sachs form factor model name in"
+    " constructor of marley::NewFormFactors" );
+
+  // Choose the model to use for the axial form factors
+  if ( axial_ff_model == "trivial" ) {
+    axial_ff_ = std::make_shared< TrivialAxialFormFactors >();
+  }
+  else if ( axial_ff_model == "dipole" ) {
+    axial_ff_ = std::make_shared< DipoleAxialFormFactors >( marley_utils::g_A,
+      marley_utils::M_A );
+  }
+  else throw marley::Error( "Unrecognized axial form factor model name in"
+    " constructor of marley::NewFormFactors" );
+}
+
+double marley::SachsFormFactors::tau( double Q2 ) {
+  return Q2 / 4. / marley_utils::m_nucleon / marley_utils::m_nucleon;
+}
+
+double marley::DipoleAxialFormFactors::FP( double Q2 ) {
+  return 2. * marley_utils::m_nucleon * this->FA( Q2 )
+    / ( marley_utils::m_pion*marley_utils::m_pion + Q2 );
+}
